6afc82666ef0fd1c46b2e37e65668d58dafab9e8
zzxming <zzxming77@163.com>
2023-07-08 16:04:28 +0800

    docs: rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	base: process.env.NODE_ENV === 'production' ? '/pic-cut/' : '/',
 	build: {
 		outDir: './docs',
 	},
